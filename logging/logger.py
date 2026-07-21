d5e9202425f95ee16646e3eedacd4f24c1d9acad
codewithtea130 <tea858880@gmail.com>
2026-07-21 17:51:33 +0530

    add ProjectLogger __init__

--- logging/logger.py
+++ logging/logger.py
@@ -3,5 +3,53 @@ import logging.handlers
 import pathlib
 import json
 import typing
+from datetime import datetime
 
 from exceptions.errors import LoggingConfigurationError
+
+
+class ProjectLogger:
+    def __init__(
+            self, 
+            log_directory: str = "logs", 
+            logger_name: str = "micrograd", 
+            console_logging: bool = True, 
+            file_logging: bool = True
+            ) -> None:
+        
+        log_directory = log_directory.strip()
+        logger_name = logger_name.strip()
+
+        if not isinstance(log_directory, str):
+            raise TypeError("log_directory must be a string")
+        
+        if not log_directory:
+            raise ValueError("log_directory cannot be empty")
+
+        if not isinstance(logger_name, str):
+            raise TypeError("logger_name must be a string")
+        
+        if not logger_name:
+            raise ValueError("logger_name cannot be empty")
+        
+        if not isinstance(console_logging, bool):
+            raise TypeError("console_logging must be a boolean")
+        
+        if not isinstance(file_logging, bool):
+            raise TypeError("file_logging must be a boolean")
+        
+        
+        self.log_directory = pathlib.Path(log_directory)
+        self.log_directory.mkdir(parents=True, exist_ok=True)
+
+        self.logger_name = logger_name
+
+        self.console_logging = console_logging
+        self.file_logging = file_logging
+
+        self.run_identifier = datetime.now().strftime("%Y%m%d_%H%M%S")
+        
+        self.json_log_path = self.log_directory / "run.json"
+        self.text_log_path = self.log_directory / "run.log"
+        
+        self.logger = logging.getLogger(self.logger_name)
