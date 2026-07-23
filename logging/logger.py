bd1e395ce20f6e6e5cfabca12ddb8ce8cc1a0189
codewithtea130 <tea858880@gmail.com>
2026-07-23 12:19:27 +0530

    add warning message logging

--- logging/logger.py
+++ logging/logger.py
@@ -143,3 +143,14 @@ class ProjectLogger:
             raise ValueError("message cannot be empty")
 
         self.logger.info(message)
+
+    def warning(self, message: str) -> None:
+        message = message.strip()
+
+        if not isinstance(message, str):
+            raise TypeError("message must be a string")
+
+        if not message:
+            raise ValueError("message cannot be empty")
+
+        self.logger.warning(message)
